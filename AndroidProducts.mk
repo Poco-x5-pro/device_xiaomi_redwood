fb5c61f172becad4f1e158db626e9813a6b4f48f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-03-17 22:23:50 +0600

    Revert "redwood: YAAP"
    This reverts commit 0aecd113224055ea1744a1ed1b5163b1e4a37466.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 #
 # SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-FileCopyrightText: Yet Another AOSP Project
+# SPDX-FileCopyrightText: PixelOS
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_redwood.mk
+    $(LOCAL_DIR)/aosp_redwood.mk
