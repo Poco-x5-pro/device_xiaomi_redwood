3cd77581927b27c33641454e050e05b467fd07c5
Thereache <thereache2003@gmail.com>
2026-04-03 10:41:16 +0530

    redwood: back to lineage
    Signed-off-by: Thereache <thereache2003@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,7 @@
 #
 # SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-FileCopyrightText: PixelOS
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/custom_redwood.mk
+    $(LOCAL_DIR)/lineage_redwood.mk
--- lineage_redwood.mk
+++ lineage_redwood.mk
@@ -1,20 +1,19 @@
 #
 # SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-FileCopyrightText: PixelOS
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from redwood device
 $(call inherit-product, device/xiaomi/redwood/device.mk)
 
-# Inherit some common PixelOS stuff
-$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+# Inherit some common lineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BRAND := POCO
 PRODUCT_DEVICE := redwood
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 22101320G
-PRODUCT_NAME := custom_redwood
+PRODUCT_NAME := lineage_redwood
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -23,6 +22,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=POCO/redwood_global/redwood:14/UKQ1.240624.001/OS2.0.17.0.UMSMIXM:user/release-keys \
     DeviceProduct=redwood \
     SystemName=redwood_global
-
-ASCP_MAINTAINER := Thereache
-ASCP_OFFICIAL := true
